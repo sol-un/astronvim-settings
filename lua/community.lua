83a43fde5924e91ef23867326a4fe21762a92fb7
solovev.aa <solovev.aa@skbkontur.ru>
2025-09-18 19:08:51 +0300

    Initial commit

---
+++ lua/community.lua
@@ -0,0 +1,15 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  -- UI
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+
+  -- Motions
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.motion.mini-ai" },
+}
---
+++ lua/plugins/sos.lua
@@ -0,0 +1,5 @@
+return {
+  "tmillr/sos.nvim",
+  event = "VeryLazy",
+  opts = { autowrite = "all", timeout = 60 * 60 * 24 * 1000 },
+}
