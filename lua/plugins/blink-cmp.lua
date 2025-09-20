835b36c0268733a47d25d399a995e771a2891f36
sol-un <solovyev833@gmail.com>
2025-09-20 15:27:43 +0300

    enable blink autocomplete in cmdline

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,9 +1,22 @@
--- Autocomplete from visible buffers in codecompanion chat
-
 return {
   "Saghen/blink.cmp",
+  event = { "CmdlineEnter" },
   opts = {
+    cmdline = {
+      enabled = true,
+      keymap = {
+        preset = "cmdline",
+      },
+      completion = {
+        list = { selection = { preselect = false } },
+        menu = {
+          auto_show = function() return vim.fn.getcmdtype() == ":" end,
+        },
+        ghost_text = { enabled = true },
+      },
+    },
     sources = {
+      -- Autocomplete from visible buffers in codecompanion chat
       per_filetype = { codecompanion = { inherit_defaults = true } },
     },
   },
