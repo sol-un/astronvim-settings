976729956ca1ba81a6ee9df5dd445b6e587d8bd5
solovev.aa <solovev.aa@skbkontur.ru>
2025-09-30 15:38:29 +0300

    set up render-markdown

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,7 @@
+---@module 'render-markdown'
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" },
+  ---@type render.md.UserConfig
+  opts = { preset = "lazy" },
+}
