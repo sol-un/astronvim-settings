9c673ae0d43503b09329863a52a4e27ff81db130
sol-un <solovyev833@gmail.com>
2025-10-21 10:02:00 +0300

    replace resession.nvim w/ mini-sessions

--- lua/plugins/git-branch-sessions.lua
+++ lua/plugins/git-branch-sessions.lua
@@ -1,11 +1,14 @@
 -- function for calculating the current session name
 local get_session_name = function()
   local name = vim.fn.getcwd()
+  local name_normalized = string.gsub(name, "/", "_")
   local branch = vim.fn.system "git branch --show-current"
+
   if vim.v.shell_error == 0 then
-    return name .. "_" .. vim.trim(branch --[[@as string]])
+    local branch_normalized = string.gsub(branch, "/", "_")
+    return name_normalized .. "_" .. vim.trim(branch_normalized --[[@as string]])
   else
-    return name
+    return name_normalized
   end
 end
 
@@ -19,15 +22,25 @@ return {
     },
     mappings = {
       n = {
+        ["<Leader>Sl"] = {
+          function()
+            local last_session = require("mini.sessions").get_latest()
+            require("mini.sessions").read(last_session)
+          end,
+          desc = "Last session",
+        },
+        ["<Leader>SD"] = {
+          function() require("mini.sessions").select "delete" end,
+          desc = "Delete session",
+        },
+        ["<Leader>SF"] = {
+          function() require("mini.sessions").select() end,
+          desc = "Select session",
+        },
         -- update save dirsession mapping to get the correct session name
         ["<Leader>SS"] = {
-          function() require("resession").save(get_session_name(), { dir = "dirsession" }) end,
-          desc = "Save this dirsession",
-        },
-        -- update load dirsession mapping to get the correct session name
-        ["<Leader>S."] = {
-          function() require("resession").load(get_session_name(), { dir = "dirsession" }) end,
-          desc = "Load current dirsession",
+          function() require("mini.sessions").write(get_session_name()) end,
+          desc = "Save this session",
         },
       },
     },
@@ -39,23 +52,25 @@ return {
           desc = "Save git branch directory sessions on close",
           callback = vim.schedule_wrap(function()
             if require("astrocore.buffer").is_valid_session() then
-              require("resession").save(get_session_name(), { dir = "dirsession", notify = false })
+              require("mini.sessions").write(get_session_name())
             end
           end),
         },
-        {
-          event = "VimEnter",
-          desc = "Save current session before loading the next one",
-          callback = function()
-            local resession = require "resession"
-            resession.add_hook("pre_load", function()
-              local info = resession.get_current_session_info()
-              if info == nil then return end
-
-              resession.save(info.name, { dir = info.dir, notify = false })
-            end)
-          end,
-        },
+        -- {
+        --   event = "User",
+        --   pattern = { "NeogitBranchCheckout" },
+        --   desc = "Switch session on neogit branch checkout",
+        --   callback = function(args)
+        --     local sessions = require("resession").list { dir = "dirsession" }
+        --     local branch_name_serialized = string.gsub(args.data.branch_name, "/", "_")
+        --
+        --     for _, session_name in ipairs(sessions) do
+        --       if string.match(session_name, branch_name_serialized) ~= nil then
+        --         require("resession").load(session_name, { dir = "dirsession", notify = true })
+        --       end
+        --     end
+        --   end,
+        -- },
       },
     },
   },
---
+++ lua/plugins/mini-sessions.lua
@@ -0,0 +1,4 @@
+return {
+  { "resession.nvim", enabled = false },
+  { "nvim-mini/mini.sessions", opts = {} },
+}
