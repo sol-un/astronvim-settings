7853a235069750500b7d26b26ad71bd6871981fe
sol-un <solovyev833@gmail.com>
2025-10-27 09:13:03 +0300

    update smear-cursor settings

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -1,1 +1,7 @@
-return { "sphamba/smear-cursor.nvim", opts = {} }
+return {
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = { min_horizontal_distance_smear = 2, min_vertical_distance_smear = 2 },
+  },
+  { "nvim-mini/mini.animate", opts = { cursor = { enable = false } } },
+}
