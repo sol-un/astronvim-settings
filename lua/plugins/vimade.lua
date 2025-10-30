763dedf32defd1cf8ca32a3f12e673d4ebda4353
sol-un <solovyev833@gmail.com>
2025-10-30 12:42:55 +0300

    disable colourful-winsep

--- lua/plugins/vimade.lua
+++ lua/plugins/vimade.lua
@@ -1,29 +1,5 @@
+-- Only use vimade to fade the entire editor when the window is blurred
 return {
-  {
-    "nvim-zh/colorful-winsep.nvim",
-    event = "VeryLazy",
-    opts = {
-      border = "rounded",
-      excluded_ft = { "NeogitPopup" },
-      highlight = function()
-        local hl = vim.api.nvim_get_hl(0, { name = "FloatBorder" })
-        vim.api.nvim_set_hl(0, "ColorfulWinSep", { fg = hl.fg })
-      end,
-      indicator_for_2wins = {
-        symbols = {
-          start_left = "󰄾",
-          end_left = "󰄾",
-          start_down = "󰄿",
-          end_down = "󰄿",
-          start_up = "󰄼",
-          end_up = "󰄼",
-          start_right = "󰄽",
-          end_right = "󰄽",
-        },
-      },
-    },
-  },
-  -- Only use vimade to fade the entire editor when the window is blurred
   {
     "TaDaa/vimade",
     event = "VeryLazy",
